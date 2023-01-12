0c9990e9835de09aa4cac83e43e381160af3f202
SamuelParker <samuel.parker@spirit-digital.co.uk>
2023-01-12 12:31:12 +0000

    DataGrid able to display data plus sql scripts

--- TrailRun/TrailRun/Form1.Designer.cs
+++ TrailRun/TrailRun/Form1.Designer.cs
@@ -30,6 +30,7 @@ namespace TrailRun
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.btnAccept1 = new System.Windows.Forms.Button();
             this.lblTimesClicked = new System.Windows.Forms.Label();
             this.btnSearchDatabase = new System.Windows.Forms.Button();
@@ -37,11 +38,32 @@ namespace TrailRun
             this.txtAge = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(481, 190);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(435, 298);
+            this.dataGridView1.TabIndex = 0;
             // 
             // btnAccept1
             // 
-            this.btnAccept1.Location = new System.Drawing.Point(85, 136);
+            this.btnAccept1.Location = new System.Drawing.Point(75, 63);
             this.btnAccept1.Name = "btnAccept1";
             this.btnAccept1.Size = new System.Drawing.Size(94, 29);
             this.btnAccept1.TabIndex = 0;
@@ -53,7 +75,7 @@ namespace TrailRun
             // 
             this.lblTimesClicked.AutoSize = true;
             this.lblTimesClicked.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblTimesClicked.Location = new System.Drawing.Point(51, 87);
+            this.lblTimesClicked.Location = new System.Drawing.Point(30, 9);
             this.lblTimesClicked.Name = "lblTimesClicked";
             this.lblTimesClicked.Size = new System.Drawing.Size(180, 37);
             this.lblTimesClicked.TabIndex = 1;
@@ -61,24 +83,24 @@ namespace TrailRun
             // 
             // btnSearchDatabase
             // 
-            this.btnSearchDatabase.Location = new System.Drawing.Point(468, 188);
+            this.btnSearchDatabase.Location = new System.Drawing.Point(757, 17);
             this.btnSearchDatabase.Name = "btnSearchDatabase";
             this.btnSearchDatabase.Size = new System.Drawing.Size(159, 29);
             this.btnSearchDatabase.TabIndex = 2;
-            this.btnSearchDatabase.Text = "Search Database";
+            this.btnSearchDatabase.Text = "Add to database";
             this.btnSearchDatabase.UseVisualStyleBackColor = true;
             this.btnSearchDatabase.Click += new System.EventHandler(this.btnSearchDatabase_Click);
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(85, 340);
+            this.txtName.Location = new System.Drawing.Point(602, 49);
             this.txtName.Name = "txtName";
             this.txtName.Size = new System.Drawing.Size(125, 27);
             this.txtName.TabIndex = 3;
             // 
             // txtAge
             // 
-            this.txtAge.Location = new System.Drawing.Point(85, 413);
+            this.txtAge.Location = new System.Drawing.Point(791, 75);
             this.txtAge.Name = "txtAge";
             this.txtAge.Size = new System.Drawing.Size(125, 27);
             this.txtAge.TabIndex = 4;
@@ -86,7 +108,7 @@ namespace TrailRun
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(101, 380);
+            this.label1.Location = new System.Drawing.Point(825, 52);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(36, 20);
             this.label1.TabIndex = 5;
@@ -95,17 +117,112 @@ namespace TrailRun
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(101, 307);
+            this.label3.Location = new System.Drawing.Point(656, 26);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(49, 20);
             this.label3.TabIndex = 7;
             this.label3.Text = "Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(166, 221);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 224);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Database Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 263);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Database Age";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 306);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Database Job";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(166, 263);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 27);
+            this.textBox2.TabIndex = 12;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(166, 306);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(125, 27);
+            this.textBox3.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 366);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Insert";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(133, 366);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(233, 366);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(333, 366);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 29);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Display";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(971, 574);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtAge);
@@ -113,8 +230,10 @@ namespace TrailRun
             this.Controls.Add(this.btnSearchDatabase);
             this.Controls.Add(this.lblTimesClicked);
             this.Controls.Add(this.btnAccept1);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,6 +241,7 @@ namespace TrailRun
 
         #endregion
 
+        private System.Windows.Forms.DataGridView dataGridView1;
         private Button btnAccept1;
         private Label lblTimesClicked;
         private Button btnSearchDatabase;
@@ -129,5 +249,15 @@ namespace TrailRun
         private TextBox txtAge;
         private Label label1;
         private Label label3;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
     }
 }
--- TrailRun/TrailRun/Form1.cs
+++ TrailRun/TrailRun/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 using System.Data.SqlTypes;
+using System.Data;
 
 namespace TrailRun
 {
@@ -21,6 +22,8 @@ namespace TrailRun
         {
             InitializeComponent();
 
+            BindGrid();
+
             string ClicksCountedText = "Times clicked: " + ClickCounter;
 
             lblTimesClicked.Text = ClicksCountedText;
@@ -28,6 +31,25 @@ namespace TrailRun
 
         public string conString = "Data Source=(localdb)\\Local;Initial Catalog=TestDatabasePratice;Integrated Security=True";
 
+        public void BindGrid()
+        {
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM People", con))
+                {
+                    cmd.CommandType = CommandType.Text;
+
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        using (DataTable dt = new DataTable())
+                        {
+                            sda.Fill(dt);
+                            dataGridView1.DataSource = dt;
+                        }
+                    }
+                }
+            }
+        }
 
         private void btnAccept1_Click(object sender, EventArgs e)
         {
@@ -41,16 +63,30 @@ namespace TrailRun
 
             lblTimesClicked.Text = ClicksCountedText;
 
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
+            if (con.State == System.Data.ConnectionState.Open)
+            {
+                string qq = "SELECT * FROM People";
+                SqlCommand cmd = new SqlCommand(qq, con);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Connection made successfully, All pulled");
+
+
+            }
+
+
+
             MessageBox.Show(pressedAcceptButtonText, pressedAcceptButtonCaption, MessageBoxButtons.OK);
         }
-        
+
         public void btnSearchDatabase_Click(object sender, EventArgs e)
         {
             SqlConnection con = new SqlConnection(conString);
             con.Open();
-            if (con.State== System.Data.ConnectionState.Open)
+            if (con.State == System.Data.ConnectionState.Open)
             {
-                string q = "insert into People(Name,Age)values('" +txtName.Text.ToString()+"','"+txtAge.Text.ToString()+"')";
+                string q = "insert into People(Name,Age)values('" + txtName.Text.ToString() + "','" + txtAge.Text.ToString() + "')";
                 SqlCommand cmd = new SqlCommand(q, con);
                 cmd.ExecuteNonQuery();
                 MessageBox.Show("Connection made successfully");
